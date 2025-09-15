fa87e8bcdd13d816265c74b67d0f1c535f42ea92
Suchikkon Naskar <suchikkonnaskar0@gmail.com>
2025-09-15 19:44:41 +0530

    second commiths dcvayf

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-import pymysql
-pymysql.install_as_MySQLdb()
+# import pymysql
+# pymysql.install_as_MySQLdb()
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
